683115c2696a5b4145ecf9751e71168137930499
Tymkiv Oleksandr <t755631@gmail.com>
2025-04-27 21:55:40 +0300

    First commit

---
+++ configserver/src/test/java/com/musicband/configserver/ConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musicband.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ tour/src/test/java/com/musicband/tour/TourApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musicband.tour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
